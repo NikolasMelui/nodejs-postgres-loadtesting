72a5f48ca2ead0e6d306cc2716f9cd7c22529083
nikolasmelui <slak@samaradom.ru>
2020-07-10 19:44:28 +0400

    Add hashPassword function to the helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,23 @@
 'use strict';
 
+const crypto = require('crypto');
+
 const getApplicationHost = (req) => req.headers.host.split(':')[0];
 const getApplicationPort = (req) => req.headers.host.split(':').pop();
 
-module.exports = { getApplicationHost, getApplicationPort };
+const hashPassword = (password) => {
+  const salt = crypto.randomBytes(16).toString('hex');
+
+  return new Promise((resolve, reject) => {
+    const hashedPassword = crypto
+      .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
+      .toString(`hex`);
+    if (!hashedPassword) reject(new Error('Error with passwords.'));
+    resolve({
+      hashedPassword,
+      salt,
+    });
+  });
+};
+
+module.exports = { getApplicationHost, getApplicationPort, hashPassword };
