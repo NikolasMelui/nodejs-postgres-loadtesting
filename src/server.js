b51427189b813b4344b39f16b34a76c30d765c3d
nikolasmelui <slak@samaradom.ru>
2020-07-10 17:52:55 +0400

    Create simple server with body parsing

--- src/server.js
+++ src/server.js
@@ -2,16 +2,29 @@
 
 const http = require('http');
 const { APPLICATION_HOST, APPLICATION_PORT } = require('./config');
-const { getApplicationHost, getApplicationPort } = require('./helpers');
+// const { getApplicationHost, getApplicationPort } = require('./helpers');
 
 http
-  .createServer((req, res) => {
-    res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
-    res.end(
-      `Hello from ${getApplicationHost(req)}:${getApplicationPort(
-        req,
-      )} and welcome to the nikolasmelui-node-boilerplate!`,
-    );
+  .createServer(async (req, res) => {
+    try {
+      req.setEncoding('utf8');
+
+      const { method } = req;
+      if (method !== 'POST')
+        throw new Error('Only POST request method available');
+
+      const body = [];
+      for await (const chunk of req) body.push(chunk);
+
+      const data = JSON.parse(body);
+      console.log(data);
+
+      res.writeHead(200, 'OK', { 'Content-Type': 'application/json' });
+      res.end('Wow!');
+    } catch (error) {
+      console.error(error);
+      res.end(error.message);
+    }
   })
   .listen(APPLICATION_PORT, APPLICATION_HOST, () =>
     console.log(
