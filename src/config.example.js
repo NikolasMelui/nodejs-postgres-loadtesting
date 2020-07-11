2755bcc067f8ac5cb95409bdbb37a38e8ba664fe
nikolasmelui <slak@samaradom.ru>
2020-07-11 17:12:15 +0400

    Move db configs to the config.js file Update the config.example.js file

--- src/config.example.js
+++ src/config.example.js
@@ -1,6 +1,13 @@
 // const config = {
 //   APPLICATION_HOST: '127.0.0.1',
 //   APPLICATION_PORT: 3000,
+//   DB_HOST: 'host',
+//   DB_PORT: 5432,
+//   DB_USER: 'user',
+//   DB_PASSWORD: 'password',
+//   DB_MAX_CLIENTS: 20,
+//   DB_IDLE_TIMEOUT: 30000,
+//   DB_CONNECTION_TIMEOUT: 2000,
 // };
 
 // module.exports = config;
