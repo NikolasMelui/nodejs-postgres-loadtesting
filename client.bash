2c276a047ddd97c9ef0eb403b12f94a6942f2ca4
nikolasmelui <slak@samaradom.ru>
2020-07-11 22:37:29 +0400

    Delete useless client.bash file

--- client.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Client mock with simple HTTP POST request with JSON data
-
-curl --header "Content-Type: application/json" \
-  --request POST \
-  -d "{\"login\":\"${1}\",\"password\":\"${1}\"}" \
-  http://localhost:3000/
