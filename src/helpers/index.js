3ce88d732e9de902603f2c3f447f128c7dc11d2a
nikolasmelui <slak@samaradom.ru>
2020-07-11 17:10:28 +0400

    Update the helper.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,19 +5,20 @@ const crypto = require('crypto');
 const getApplicationHost = (req) => req.headers.host.split(':')[0];
 const getApplicationPort = (req) => req.headers.host.split(':').pop();
 
-const hashPassword = (password) => {
-  const salt = crypto.randomBytes(16).toString('hex');
+const generateSalt = () => crypto.randomBytes(16).toString('hex');
 
-  return new Promise((resolve, reject) => {
+const hashPassword = (password, salt) =>
+  new Promise((resolve, reject) => {
     const hashedPassword = crypto
       .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
       .toString(`hex`);
     if (!hashedPassword) reject(new Error('Error with passwords.'));
-    resolve({
-      hashedPassword,
-      salt,
-    });
+    resolve(hashedPassword);
   });
-};
 
-module.exports = { getApplicationHost, getApplicationPort, hashPassword };
+module.exports = {
+  getApplicationHost,
+  getApplicationPort,
+  generateSalt,
+  hashPassword,
+};
